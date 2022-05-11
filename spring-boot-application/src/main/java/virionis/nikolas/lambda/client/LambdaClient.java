c21df2c7affc90844582b7f849eb33fe34dabb45
nikolas-virionis <nikkolas.nickk@gmail.com>
2022-05-10 22:40:27 -0300

    feign general fixes

--- spring-boot-application/src/main/java/virionis/nikolas/lambda/client/LambdaClient.java
+++ spring-boot-application/src/main/java/virionis/nikolas/lambda/client/LambdaClient.java
@@ -6,12 +6,12 @@ import org.springframework.web.bind.annotation.RequestBody;
 
 import virionis.nikolas.lambda.model.Test;
 
-@FeignClient(value = "lambda", url="https://gm244752ibxdmdib7ojuwbwpbe0hedlf.lambda-url.us-east-1.on.aws/")
+@FeignClient(value = "lambda", url="https://suc2fpm2qybgyvjzj4mftodsva0fvhgz.lambda-url.us-east-1.on.aws")
 public interface LambdaClient {
     // Lambda Function URL (can get it in the AWS Lambda console)
     // String url = "<LAMBDA FUNCTION URL>";
 
-    @PostMapping("")
+    @PostMapping
     Test test(@RequestBody Test test);
 
 }
